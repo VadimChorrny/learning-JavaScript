b73f47ebf93218f509a6f66e927501b13f23d116
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-07-30 18:48:04 +0300

    funny project

---
+++ Funny Project/botebot.js
@@ -0,0 +1,12 @@
+// function sayHello() {
+//     let name = prompt('What is your name?', '');
+//     alert('Hello ' + name + '!'); // Обєднав :)
+// };
+
+
+function HowAreYou() {
+    let ask = promt('How are you?', '');
+    alert(ask);
+};
+
+HowAreYou();
